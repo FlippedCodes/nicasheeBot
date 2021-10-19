6d737160331d69eafe4f63ef598a1d5ab123ae5c
Phil <philip.schaefer0@gmail.com>
2021-10-19 02:32:21 +0200

    overall cleanup and globaling

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 // init Discord
 const { Client, Intents, Collection } = require('discord.js');
+// init file system
+const fs = require('fs');
 // init command builder
 const { SlashCommandBuilder } = require('@discordjs/builders');
+// setting global values
 // init Discord client
-const client = new Client({ disableEveryone: true, intents: [Intents.FLAGS.GUILDS] });
+global.client = new Client({ disableEveryone: true, intents: [Intents.FLAGS.GUILDS] });
 // init config
-const config = require('./config.json');
+global.config = require('./config.json');
 
-client.commands = new Collection();
+global.DEBUG = process.env.NODE_ENV === 'development';
 
 global.CmdBuilder = SlashCommandBuilder;
+
+// creating collections
+client.commands = new Collection();
+client.functions = new Collection();
 
 // Login the bot
 client.login(process.env.DCtoken)
   .then(() => {
     // import Functions and Commands
-    config.setup.startupFunctions.forEach((FCN) => {
-      const INIT = require(`./functions/${FCN}.js`);
-      INIT.run(client, config);
+    fs.readdirSync('./functions/STARTUP').forEach((FCN) => {
+      if (FCN.search('.js') === -1) return;
+      const INIT = require(`./functions/STARTUP/${FCN}`);
+      INIT.run(fs);
     });
   });
 
 client.once('ready', () => {
   // confirm user logged in
   console.log(`[${config.name}] Logged in as "${client.user.tag}"!`);
+  // set bot player status
+  config.setup.setupFunctions.forEach((FCN) => {
+    client.functions.get(FCN).run(client, config);
+  });
 });
 
 client.on('interactionCreate', async (interaction) => {

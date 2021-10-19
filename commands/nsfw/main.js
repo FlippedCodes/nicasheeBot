b6257cb908b7ba3475836807c15ceb9b2b5a75d1
Phil <philip.schaefer0@gmail.com>
2021-10-19 02:31:01 +0200

    pruned test code

--- commands/nsfw/main.js
+++ commands/nsfw/main.js
@@ -1,40 +1,6 @@
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+module.exports.run = async (interaction) => {
 
-// Ping kickoff for bot latency
-async function kickoff(interaction) {
-  const sendMessage = await new MessageEmbed()
-    .setDescription('📤 Pong...')
-    .setColor();
-  const sentMessage = await interaction.reply({ embeds: [sendMessage], fetchReply: true });
-  return sentMessage;
-}
-
-// message for data return
-function editedMessage(sentMessage, interaction) {
-  const api_latency = Math.round(sentMessage.client.ws.ping);
-  const body = `📥 Pong!
-  Bot latency is \`${sentMessage.createdTimestamp - interaction.createdTimestamp}\`ms.
-  API latency is \`${api_latency}\`ms`;
-  return new MessageEmbed()
-    .setDescription(body)
-    .setColor();
-}
-
-// posts ping message and edits it afterwards
-async function checkPing(interaction) {
-  const sentReply = await kickoff(interaction);
-  const test = new MessageActionRow()
-    .addComponents(
-      new MessageButton()
-        .setCustomId('test')
-        .setEmoji('💩')
-        .setLabel('Testo')
-        .setStyle('DANGER'),
-    );
-  interaction.editReply({ embeds: [editedMessage(sentReply, interaction)], components: [test] });
-}
-
-module.exports.run = async (client, interaction) => checkPing(interaction);
+};
 
 module.exports.data = new CmdBuilder()
   .setName('nsfw')
@@ -53,7 +19,7 @@ module.exports.data = new CmdBuilder()
     .setName('change')
     .setDescription('Change the DoB of an entry.')
     .addUserOption((option) => option.setName('user').setDescription('Provide a user to to add.').setRequired(true))
-    .addStringOption((option) => option.setName('date').setDescription('Provide the users DoB.').setRequired(false)))
+    .addStringOption((option) => option.setName('date').setDescription('Provide the users DoB.').setRequired(true)))
   .addSubcommand((subcommand) => subcommand
     .setName('search')
     .setDescription('Search an entry.')

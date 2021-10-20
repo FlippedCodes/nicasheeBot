80a45e2310b019ee26c444c199af360f5410311c
Phil <philip.schaefer0@gmail.com>
2021-10-21 00:34:38 +0200

    changed to asyncronus function

--- functions/SETUP/status.js
+++ functions/SETUP/status.js
@@ -1,6 +1,9 @@
 module.exports.run = async () => {
   if (DEBUG) return;
   console.log(`[${module.exports.data.name}] Setting status...`);
+  await client.user.setStatus('online');
+  const membercount = await client.guilds.cache.reduce((previousCount, currentGuild) => previousCount + currentGuild.memberCount, 0);
+  await client.user.setActivity(`${membercount} members in VoreNetwork`, { type: 'WATCHING' });
   console.log(`[${module.exports.data.name}] Status set!`);
 };
 

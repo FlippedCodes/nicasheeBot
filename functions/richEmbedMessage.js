733ada3523429285a2b912e9a40482048907c014
Phil <philip.schaefer0@gmail.com>
2021-10-25 12:05:15 +0200

    added ephemeral support

--- functions/richEmbedMessage.js
+++ functions/richEmbedMessage.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 
-module.exports.run = async (interaction, body, title, color, footer) => {
+module.exports.run = async (interaction, body, title, color, footer, ephemeral) => {
   // needs to be local as settings overlap from dofferent embed-requests
   const embed = new MessageEmbed();
 
@@ -9,7 +9,10 @@ module.exports.run = async (interaction, body, title, color, footer) => {
   if (color) embed.setColor(color);
   if (footer) embed.setFooter(footer);
 
-  return interaction.reply({ embeds: [embed], ephemeral: true });
+  return interaction.reply({
+    embeds: [embed],
+    ephemeral: String(ephemeral) ? ephemeral : true,
+  });
 };
 
 module.exports.data = {

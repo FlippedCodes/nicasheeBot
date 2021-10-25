77b124584428d954eef7510017b46516bbacd4c7
Phil <philip.schaefer0@gmail.com>
2021-10-25 12:05:51 +0200

    populated with code

--- commands/nsfw/search.js
+++ commands/nsfw/search.js
@@ -1,5 +1,50 @@
-module.exports.run = async (interaction) => {
+const userDoB = require('../../database/models/UserDoB');
 
+async function searchUser(ID) {
+  const result = await userDoB.findOne({ where: { ID } }).catch(ERR);
+  return result;
+}
+
+function sendMessage(MessageEmbed, interaction, userTag, userID, age, DoB, allow, teammemberTag, updated, created) {
+  let color = 16741376;
+  if (allow) color = 4296754;
+
+  const embed = new MessageEmbed()
+    .setColor(color)
+    .setTitle(`${userTag}`)
+    .addFields([
+      { name: 'ID', value: userID, inline: true },
+      { name: 'Age', value: String(age), inline: true },
+      { name: 'DoB', value: DoB, inline: true },
+      { name: 'Allow', value: prettyCheck(allow), inline: true },
+      { name: 'Created by', value: teammemberTag, inline: true },
+      { name: 'Created at', value: created, inline: false },
+      { name: 'Updated at', value: updated, inline: false },
+    ]);
+
+  // send message
+  interaction.reply({ embeds: [embed] });
+}
+
+module.exports.run = async (interaction, moment, MessageEmbed) => {
+  const command = interaction.options;
+  // get user and ID
+  const user = command.getUser('user', true);
+  const userID = user.id;
+  // search entry
+  const DBentry = await searchUser(userID);
+  // report to user if entry added
+  if (!DBentry) return messageFail(interaction, `No data found for the ID \`${userID}\` (\`${user.tag}\`)!`);
+  // get DoB
+  const DoB = DBentry.DoB;
+  // get age
+  const age = moment().diff(DoB, 'years');
+  // get user tags and format dates
+  const teammemberTag = client.users.cache.find(({ id }) => id === DBentry.teammemberID).tag;
+  const [updatedAt, createdAt] = [DBentry.updatedAt, DBentry.createdAt].map((date) => moment(date).format('ddd, MMM Do YYYY, h:mm a'));
+  const formatDoB = moment(DoB).format(config.DoBchecking.dateFormats[0]);
+  // send it
+  sendMessage(MessageEmbed, interaction, user.tag, userID, age, formatDoB, DBentry.allow, teammemberTag, updatedAt, createdAt);
 };
 
 module.exports.data = { subcommand: true };

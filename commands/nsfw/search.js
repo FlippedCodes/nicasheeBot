d18494f8f9ada4272c5f2071caa88d89fb8998cd
Phil <philip.schaefer0@gmail.com>
2021-11-23 00:51:51 +0100

    use of other function to fetch user better

--- commands/nsfw/search.js
+++ commands/nsfw/search.js
@@ -40,7 +40,7 @@ module.exports.run = async (interaction, moment, MessageEmbed) => {
   // get age
   const age = moment().diff(DoB, 'years');
   // get user tags and format dates
-  const teammember = client.users.cache.find(({ id }) => id === DBentry.teammemberID);
+  const teammember = client.users.fetch(DBentry.teammemberID);
   const teammemberTag = teammember ? teammember.tag : 'none';
   const [updatedAt, createdAt] = [DBentry.updatedAt, DBentry.createdAt].map((date) => moment(date).format('ddd, MMM Do YYYY, h:mm a'));
   const formatDoB = moment(DoB).format(config.DoBchecking.dateFormats[0]);

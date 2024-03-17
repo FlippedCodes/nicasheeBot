0a899333380553259e66b7e6008b83151a65d4d9
Phil <philip.schaefer0@gmail.com>
2024-03-17 20:58:46 +0100

    fix edge case when sue ros not on server anymore

--- functions/ENGINE/checkin/COMPONENT/button/deny.js
+++ functions/ENGINE/checkin/COMPONENT/button/deny.js
@@ -10,14 +10,16 @@ module.exports.run = async (interaction) => {
   await interaction.deferUpdate();
 
   const checkinChannel = interaction.channel;
-  const userID = checkinChannel.name;
-  const user = await interaction.guild.members.fetch(userID);
-  user.send({ embeds: [embed] }).catch((e) => null);
 
   await client.functions.get('ENGINE_checkin_transcriptChannel').run(checkinChannel);
 
-  await user.kick('Checkin Denied');
-  // channel deletion is handled in member remove event
+  const userID = checkinChannel.name;
+  const user = await interaction.guild.members.fetch(userID).catch((e) => null);
+  if (user) {
+    user.send({ embeds: [embed] }).catch((e) => null);
+    await user.kick('Checkin Denied');
+    // channel deletion is handled in member remove event
+  }
 };
 
 module.exports.data = {

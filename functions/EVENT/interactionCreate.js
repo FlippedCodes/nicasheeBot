659527082d60a8204c20eecfbb1bf2eab4095f31
Phil <philip.schaefer0@gmail.com>
2026-05-29 14:45:19 +0200

    add commandahnder

--- functions/EVENT/interactionCreate.js
+++ functions/EVENT/interactionCreate.js
@@ -7,7 +7,8 @@ module.exports.run = async (interaction) => {
   // command handler
   if (interaction.isCommand()) return client.functions.get('EVENT_interaction_isCommand').run(interaction).catch(ERR);
   if (interaction.isButton()) return client.functions.get('EVENT_interaction_isButton').run(interaction).catch(ERR);
-  // if (interaction.isModalSubmit()) return client.functions.get('EVENT_interaction_isModalSubmit').run(interaction).catch(ERR);
+  if (interaction.isModalSubmit()) return client.functions.get('EVENT_interaction_isModalSubmit').run(interaction).catch(ERR);
+  if (interaction.isStringSelectMenu()) return client.functions.get('EVENT_interaction_isStringSelectMenu').run(interaction).catch(ERR);
 };
 
 module.exports.data = {

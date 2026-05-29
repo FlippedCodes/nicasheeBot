2350ad5daf0d76db8d01c94670d3e1b5f04319f1
Phil <philip.schaefer0@gmail.com>
2026-05-29 14:05:34 +0200

    moved

--- functions/reportButton.js
+++ functions/reportButton.js
@@ -9,15 +9,20 @@ const select = new ActionRowBuilder()
       .setPlaceholder('Open Report')
       .addOptions(
         new StringSelectMenuOptionBuilder()
+          .setLabel('Feedback/Suggestion')
+          .setEmoji('⭐')
+          .setDescription('Do you have a suggestion of feedback for us? Feel free to share them!')
+          .setValue('suggestion'),
+        new StringSelectMenuOptionBuilder()
           .setLabel('User in text')
           .setEmoji('🔤')
           .setDescription('I want to report something happened in text.')
           .setValue('userText'),
         new StringSelectMenuOptionBuilder()
-          .setLabel('User in DMs')
+          .setLabel('User in Meetup')
           .setEmoji('📨')
-          .setDescription('I want to report a user in my DMs.')
-          .setValue('userDM'),
+          .setDescription('I want to report a user in a Furbulous Meetup.')
+          .setValue('userMeet'),
         new StringSelectMenuOptionBuilder()
           .setLabel('User in voice')
           .setEmoji('🔊')
@@ -43,8 +48,8 @@ module.exports.run = async () => {
   await reportChannel.bulkDelete(1).catch(ERR);
   const embed = new EmbedBuilder()
     .setColor('Red')
-    .setTitle('Report System')
-    .setDescription('Hello!\nIn this channel, we manage our user reports. If you want to open up a report, you may use the selection menu below for the right form.\n**If applicable, make sure you have a message link ready!**')
+    .setTitle('Ticket System')
+    .setDescription('Hello!\nIn this channel, we manage our user reports and accept suggestions. If you want to open up a ticket, you may use the selection menu below for the right form.\n**If applicable, make sure you have a message link ready!**')
     .setFooter({ text: 'One issue per ticket! Abuse of the report system will result in punishment!' });
   reportChannel.send({ embeds: [embed], components: [select] });
 };
